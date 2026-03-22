08fc5eed943c0c5222f0eeab7ce14ed0b1909ce3
Cerra1024 <sangarihar@gmail.com>
2026-03-22 19:59:37 -0400

    Fixed syntax errors in app.js

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-let task = [];
+let tasks = [];
 
 const taskNameInput = document.getElementById("taskName");
 const categoryInput = document.getElementById("category");
@@ -7,7 +7,7 @@ const statusInput = document.getElementById("status");
 const addTaskBtn = document.getElementById("addTaskBtn");
 const taskList = document.getElementById("taskList");
 
-addTaskBtn.addEventListener("click," addTask);
+addTaskBtn.addEventListener("click", addTask);
 
 function addTask() {
     const name = taskNameInput.value;
@@ -20,10 +20,49 @@ function addTask() {
         return;
     }
 
-    const task == {
+    const task = {
         name: name,
-        catergory: category,
+        category: category,
         deadline: deadline,
-        status status
+        status: status
     };
+
+    tasks.push(task);
+
+    renderTasks();
+    clearInputs();
+
+}
+
+    function clearInputs() {
+        taskNameInput.value = "";
+        categoryInput.value = "";
+        deadlineInput.value = "";
+        statusInput.value = "In Progress";
+    }
+
+    function renderTasks() {
+        taskList.innerHTML = "";
+
+        tasks.forEach((task,index) => {
+            const row = document.createElement("tr");
+
+            row.innerHTML = `
+            <td>${task.name}</td>
+            <td>${task.category}</td>
+            <td>${task.deadline}</td>
+            <td>${task.status}</td>
+            <td>
+                <button onclick="deleteTask(${index})">Delete</button>
+                </td> `;
+
+            taskList.appendChild(row);
+
+        });
+
+    }
+
+function deleteTask(index) {
+    tasks.splice(index, 1);
+    renderTasks();
 }
